792b71bdcb6792b524fdfca94fbf2d80e6a4dca8
andythepee <131628979+andythepee@users.noreply.github.com>
2023-05-26 00:55:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown');
+const generateMarkdown = require('./generateMarkdown');
 
 // Array of questions for user input
 const questions = [
